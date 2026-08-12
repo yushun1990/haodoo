5f6ca0b56a7cd35006350747f87898a00211cab4
yushun1990 <yushun1990@gmail.com>
2026-08-12 23:38:09 +0800

    fix: apply mobile layout overrides

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import './styles.css'
 import './parts.css'
 import './reader.css'
+import './mobile.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
